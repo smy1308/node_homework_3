8f6adddf7e553a9c97f8f4255fbd4970fddf5ec1
smy1308 <smy1308@gmail.com>
2023-12-09 02:31:49 +0900

    Feat:미들웨어 추가
    - middleware폴더에 log.middleware파일 추가.
    - middleware폴더에 erro-handler파일 추가.
    - middleware거치도록 app.js 수정.
    - 썬더 클라이언트 테스트 완료.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import UsersRouter from './routers/users.router.js';
+import logMiddleware from './middlewares/log.middleware.js';
+import errorHandlerMiddleware from './middlewares/error-handler.middleware.js.js';
 
 const app = express();
 const PORT = 3018;
 
+app.use(logMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api', [UsersRouter]);
+app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -8,35 +8,39 @@ const router = express.Router();
 
 /** 회원가입 API **/
 router.post('/sign-up', async (req, res, next) => {
-  const { email, password, name, age, gender, profileImage } = req.body;
-  const isExistUser = await prisma.users.findFirst({
-    where: {
-      email,
-    },
-  });
-
-  if (isExistUser) {
-    return res.status(409).json({ message: '이미 존재하는 이메일입니다.' });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  // Users 테이블에 사용자를 추가합니다.
-  const user = await prisma.users.create({
-    data: { email, password: hashedPassword },
-  });
-  // UserInfos 테이블에 사용자 정보를 추가합니다.
-  const userInfo = await prisma.userInfos.create({
-    data: {
-      UserId: user.userId,
-      name,
-      age,
-      gender: gender.toUpperCase(),
-      profileImage,
-    },
-  });
-
-  return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+  try {
+    const { email, password, name, age, gender, profileImage } = req.body;
+    const isExistUser = await prisma.users.findFirst({
+      where: {
+        email,
+      },
+    });
+    
+    if (isExistUser) {
+      return res.status(409).json({ message: '이미 존재하는 이메일입니다.' });
+    }
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    // Users 테이블에 사용자를 추가합니다.
+    const user = await prisma.users.create({
+      data: { email, password: hashedPassword },
+    });
+    // UserInfos 테이블에 사용자 정보를 추가합니다.
+    const userInfo = await prisma.userInfos.create({
+      data: {
+        UserId: user.userId,
+        name,
+        age,
+        gender: gender.toUpperCase(),
+        profileImage,
+      },
+    });
+    
+    return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+  } catch (error) {
+    next(err);
+  };
 });
 
 /** 로그인 API **/
