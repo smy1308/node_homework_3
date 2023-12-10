6c22b32edf921534710772b4b0353199c0075929
smy1308 <smy1308@gmail.com>
2023-12-10 17:04:46 +0900

    Style:불필요한 코드 삭제
    - users.router에 JWT 삭제.

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import { prisma } from '../utils/prisma/index.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 import { Prisma } from '@prisma/client';
